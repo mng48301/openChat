20a3be427697e010778451f11672071de0f23cef
mng48301 <mng48301@gmail.com>
2025-03-20 13:49:55 -0400

    test_branch

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,10 @@
 package client;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
 
+// Client class
 
 public class Client extends Application {
     public static void main(String[] args) {
@@ -11,6 +14,13 @@ public class Client extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Client");
+        Button button = new Button("Test");
+        button.setOnAction(e -> System.out.println("Test"));
+        Scene scene = new Scene(button, 300, 300);
+        primaryStage.setScene(scene);
+
+
+
         primaryStage.show();
     }
 }
